891833540fa53415efa9ef6dec7c4b2e89c4c837
Naza1re <mcarim@mail.ru>
2024-09-14 22:38:00 +0300

    add : bpmn-task-process-service for tracking status of task on camunda

---
+++ bpmn-task-process/src/main/java/org/example/bpmntaskprocess/BpmnTaskProcessApplication.java
@@ -0,0 +1,13 @@
+package org.example.bpmntaskprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnTaskProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmnTaskProcessApplication.class, args);
+	}
+
+}
---
+++ bpmn-task-process/src/test/java/org/example/bpmntaskprocess/BpmnTaskProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bpmntaskprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmnTaskProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
